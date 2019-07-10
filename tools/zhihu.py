9b024e9ba5cd9d8af7f857ae98287d5623668a9d
cxy <chenxuanye@mrglee.com>
2019-07-10 20:16:26 +0800

    添加知乎导出工具

--- tools/zhihu.py
+++ tools/zhihu.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python3
+
 import os
 import glob
 import re
 
-# <img src="https://www.zhihu.com/equation?tex=\1" alt="\1" class="ee_img tr_noresize" eeimg="1">
-# \n<img src="https://www.zhihu.com/equation?tex=\1" alt="\1" class="ee_img tr_noresize" eeimg="1">\n
+pattern = re.compile(r"\$(.+?)\$", re.M)
+pattern2 = re.compile(r"\$\$(.+?)\$\$", re.M)
+
 file_dir = os.path.dirname(os.path.realpath(__file__))
 root_dir = os.path.join(file_dir, "..")
 dist_dir = os.path.join(root_dir, "dist")
+
 
 def clear_dir(dir):
     for root, dirs, files in os.walk(dir):
@@ -15,6 +19,17 @@ def clear_dir(dir):
         for name in dirs:
             os.rmdir(os.path.join(root, name))
 
+
+def repl(match):
+    group = match.group(1)
+    return '<img src="https://www.zhihu.com/equation?tex=%s" alt="%s" class="ee_img tr_noresize" eeimg="1">' % (group, group)
+
+
+def repl2(match):
+    group = match.group(1)
+    return '\n<img src="https://www.zhihu.com/equation?tex=%s" alt="%s" class="ee_img tr_noresize" eeimg="1">\n' % (group, group)
+
+
 def main():
     if (not os.path.exists(dist_dir)):
         os.mkdir(dist_dir)
@@ -22,12 +37,11 @@ def main():
     clear_dir(dist_dir)
 
     for filename in glob.glob(os.path.join(root_dir, "docs/*")):
-        with open(filename, "r") as fr:
-            with open(os.path.join(dist_dir, os.path.basename(filename)), "w") as fw:
-                # fw.write(fr.read())
-                print(fr.read())
+        with open(filename, "r", encoding="utf8") as fr:
+            with open(os.path.join(dist_dir, os.path.basename(filename)), "w", encoding="utf8") as fw:
+                fw.write(pattern.sub(repl, pattern2.sub(repl2, fr.read())))
 
- 
+
 if __name__ == "__main__":
     try:
         main()
